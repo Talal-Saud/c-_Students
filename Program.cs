0e37e6d97b1f995a1adfe36ae6f149a0bbf69d9e
Talal-Saud <42378935+Talal-Saud@users.noreply.github.com>
2021-09-06 21:19:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Project_Students
+{
+    class Project_Students
+    {
+
+        static void Main(string[] args)
+        {
+            int number;
+            do
+            {
+                Console.WriteLine("Enter number Students : ");
+                number = int.Parse(Console.ReadLine());
+            }
+            while (number <= 0);
+
+
+            string[] name = new string[number];
+            int[] mark = new int[number];
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Enter Name : ");
+                name[i] = Console.ReadLine();
+                Console.WriteLine("Enter Mark : ");
+                mark[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("==============================");
+
+            for (int i = 0; i < number; i++)
+            {
+                if (mark[i] >= 60)
+                {
+                    Console.WriteLine(" {0} is Passed ", name[i]);
+                }
+                else if (mark[i] < 60)
+                {
+                    Console.WriteLine(" {0} is Failed  ", name[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Not Found !");
+                }
+            }
+
+
+
+        }
+
+
+    }
+}
